b61f286dbd5b4ac2899c6425975fa1cfe4faddc8
Unknown <misterplaymania@gmail.com>
2018-06-22 11:15:33 +0200

    Question 2

---
+++ Isen.meziane.Library/INode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isen.meziane.Library
+{
+    public interface INode<T>
+    {
+        /// summary
+        /// Renvoie la string Value
+        T value { get; set; }
+        
+        Guid Id { get; }
+        
+        Node<T> Parent { get; set; }
+        
+        List<Node<T>> Children { get; set; }
+        
+        int Depth { get; set; }
+    }
+    
+}
--- Isen.meziane.Library/Node.cs
+++ Isen.meziane.Library/Node.cs
@@ -1,7 +1,41 @@
-﻿namespace Isen.meziane.Library
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isen.meziane.Library
 {
-    public class Node
+    public class Node<T>
+            : INode<T> , IEquatable<Node<T>>
     {
+        //Rider auto-generate code
+        public T value { get; set; }
+        public Guid Id { get; }
+        public Node<T> Parent { get; set; }
+        public List<Node<T>> Children { get; set; }
+        public int Depth { get; set; }
+
         
+        //Rider auto-generate code
+        public bool Equals(Node<T> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return EqualityComparer<T>.Default.Equals(value, other.value) && Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Node<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (EqualityComparer<T>.Default.GetHashCode(value) * 397) ^ Id.GetHashCode();
+            }
+        }
     }
 }
